fe93fa0481241982b879d9122c2b23cf795f4afc
MatHyp <matijoker29@gmail.com>
2022-09-03 15:56:35 +0200

    init commit

---
+++ next/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Technikum Technologii Cyfrowych</title>
+        <meta name="description" content="Strona z informacjami na temat Technikum Technologi Cyfrowych" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+    </div>
+  )
+}
